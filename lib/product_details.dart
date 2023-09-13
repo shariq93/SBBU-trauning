349d07ecf135a54663e478d2f4db2ca30f26d72d
Shariq Khan <shariqsb93@gmail.com>
2023-09-13 11:03:52 +0500

    Api details integration

--- lib/product_details.dart
+++ lib/product_details.dart
@@ -1,27 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:http/http.dart' as http;
 
-class ProductDetails extends StatelessWidget {
-  ProductDetails({super.key});
+import 'model/product_model.dart';
 
-  final List images = [
-    {
-      "image":
-          "https://www.demo.martvill.techvill.net/public/uploads/20221123/f8256ae374c1432ad6b8ac1e81fde25e.jpg",
-      "title": "My Sofa"
-    },
-    {
-      "image":
-          "https://www.demo.martvill.techvill.net/public/uploads/20221123/8455776f6791cd5479bb9a88c829876c.png",
-      "title": "My Sofa 2"
-    },
-    {
-      "image":
-          "https://www.demo.martvill.techvill.net/public/uploads/sizes/medium/20221116/8e4f7f9cffd21f3f267147d2cf3e79b1.jpg",
-      "title": "My Key Chains"
-    },
-  ];
+class ProductDetails extends StatefulWidget {
+  const ProductDetails({super.key});
 
+  @override
+  State<ProductDetails> createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  Product product = Product(title: "", image: "", price: "");
+
+  getProductDetails(String id) async {
+    var url = Uri.parse("https://fakestoreapi.com/products/$id");
+
+    dynamic response = await http.get(url);
+
+    var data = jsonDecode(response.body);
+    setState(() {
+      product = Product(title: data['title'], image: data['image'], price: "");
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getProductDetails("10");
+    super.initState();
+  }
+
+  var PLACEHOLEDR_IMAGE =
+      "https://t3.ftcdn.net/jpg/03/45/05/92/360_F_345059232_CPieT8RIWOUk4JqBkkWkIETYAkmz2b75.jpg";
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -30,50 +44,17 @@ class ProductDetails extends StatelessWidget {
           title: Text("Product Details"),
         ),
         body: Container(
-          color: Colors.grey.shade300,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              CarouselSlider(
-                options: CarouselOptions(
-                    height: 240.0,
-                    autoPlay: true,
-                    enlargeCenterPage: true,
-                    enlargeFactor: 0.3,
-                    autoPlayInterval: Duration(seconds: 2),
-                    autoPlayAnimationDuration: Duration(milliseconds: 2000)),
-                items: images.map((item) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 0.0),
-                        decoration: BoxDecoration(
-                          color: Colors.transparent,
-                        ),
-                        child: Column(
-                          children: [
-                            Image.network(
-                              item['image'],
-                              height: 200,
-                              width: MediaQuery.of(context).size.width,
-                              fit: BoxFit.cover,
-                            ),
-                            Text(
-                              item["title"],
-                              style: TextStyle(fontSize: 20),
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-              )
-            ],
-          ),
+          child: Column(children: [
+            Image.network(
+              (product.image == '' ? PLACEHOLEDR_IMAGE : product.image),
+              width: double.infinity,
+              height: 300,
+            ),
+            Text(
+              product.title,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ]),
         ),
       ),
     );
